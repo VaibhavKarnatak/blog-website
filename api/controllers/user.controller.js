13e6fe6b991cde315e9d8007a549105cf0b39777
HeatblasterAA <amanarora_60@yahoo.com>
2024-07-24 20:00:05 +0530

    Added update user API route...

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,3 +1,62 @@
-export const test = (req,res) => {
-    res.json({message: 'API is working'});
+import User from "../models/user.models.js";
+import { errorHandler } from "../utils/error.js";
+import bcryptjs from "bcryptjs";
+
+export const test = (req, res) => {
+  res.json({ message: "API is working" });
+};
+
+export const updateUser = async (req, res, next) => {
+  //checking if the userId matches
+  if (req.user.id !== req.params.userId) {
+    return next(errorHandler(403, "You are not allowed to update this user"));
+  }
+  //update password
+  if (req.body.password) {
+    if (req.body.password.length < 6) {
+      return next(
+        errorHandler(400, "Password must be at least 6 characters long")
+      );
+    }
+
+    req.body.password = bcryptjs.hashSync(req.body.password, 10);
+  }
+  //updating USERNAME
+  if (req.body.username) {
+    if (req.body.username.length < 7 || req.body.username > 20) {
+      return next(
+        errorHandler(400, "Username should be between 7 to 20 characters")
+      );
+    }
+    if (req.body.username.includes(" ")) {
+      return next(errorHandler(400, "Username cannot contain spaces"));
+    }
+    if (req.body.username !== req.body.username.toLowerCase()) {
+      return next(errorHandler(400, "Username must be lowercase"));
+    }
+    if (!req.body.username.match(/^[a-zA-Z0-9]+$/)) {
+      return next(
+        errorHandler(400, "Username can only contain letters and numbers")
+      );
+    }
+
+    try {
+      const updateUser = await User.findByIdAndUpdate(
+        req.params.userId,
+        {
+          $set: {
+            username: req.body.username,
+            email: req.body.password,
+            profilePicture: req.body.profilePicture,
+            password: req.body.password,
+          },
+        },
+        { new: true }
+      );
+      const { password, ...rest } = updateUser._doc;
+      res.status(200).json(rest);
+    } catch (error) {
+      next(error);
+    }
+  }
 };
--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -3,65 +3,61 @@ import React, { useEffect, useRef, useState } from 'react'
 import { useSelector } from 'react-redux'
 
 export default function DashProfile() {
-    const {currentUser} = useSelector(state => state.user)
-    const {imageFile,setImagefile} = useState(null);
-    const {imageFileUrl,setImagefileUrl} = useState(null);
-    const filePickerRef = useRef();
-    const handleImagechange = (e) => {
-        const file = e.target.files[0];
-        if(file)
-            {
-                setImagefile(file);
-                setImagefileUrl(URL.createObjectURL(file));
-            }
-    };
-    useEffect(() => {
-        if(imageFile) {
-          uploadImage();  
-        }
-    },[imageFile]);
+  const { currentUser } = useSelector(state => state.user)
+  const [imageFile, setImagefile] = useState(null);
+  const [imageFileUrl, setImagefileUrl] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+
+    if (file) {
+      setImagefile(file);
+      setImagefileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile) {
+      // Add your upload logic here
+    }
+  }, [imageFile]);
+
   return (
     <div className='max-w-lg mx-auto p-3 w-full'>
       <h1 className='my-7 text-center font-semibold text-3xl'>Profile</h1>
-
       <form className='flex flex-col gap-4'>
-
-        <input type="file" accept='image/*' onChange={handleImagechange} ref={filePickerRef}
-        hidden />
-
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden />
         <div className='w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full' onClick={() => filePickerRef.current.click()}>
-
-        <img src={ imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
-
+          <img src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[lightgray]' />
         </div>
-        <TextInput type='text'
-         id='username'
-          placeholder='username' 
-          defaultValue={currentUser.username} 
-          
-          />
-
-        <TextInput type='email'
-         id='email'
-          placeholder='email' 
-          defaultValue={currentUser.email} 
-          />
-
-        <TextInput type='password'
-         id='password'
-          placeholder='password' 
-          />
-
-          <Button type='submit' gradientDuoTone='purpleToBlue' outline >
-            Update
-          </Button>
+        <TextInput
+          type='text'
+          id='username'
+          placeholder='username'
+          defaultValue={currentUser.username}
+        />
+        <TextInput
+          type='email'
+          id='email'
+          placeholder='email'
+          defaultValue={currentUser.email}
+        />
+        <TextInput
+          type='password'
+          id='password'
+          placeholder='password'
+        />
+        <Button type='submit' gradientDuoTone='purpleToBlue' outline>
+          Update
+        </Button>
       </form>
       <div className="text-red-500 flex justify-between mt-5">
         <span className='cursor-pointer'>
-            Delete Account
+          Delete Account
         </span>
         <span className='cursor-pointer'>
-            Sign Out
+          Sign Out
         </span>
       </div>
     </div>
