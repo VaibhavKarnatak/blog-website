252216b95235f0e775594c51bad3161eae23738f
HeatblasterAA <amanarora_60@yahoo.com>
2024-07-25 19:29:49 +0530

    added admin functionality to the user

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -7,38 +7,35 @@ import OAuth from '../components/OAuth';
 export default function SignUp() {
   const [formData, setFormData] = useState({});
   const [errorMessage, setErrorMessage] = useState(null);
-  const [loading,setLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
   const handleChange = (e) => {
-    setFormData({...formData,[e.target.id]: e.target.value.trim() });
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    if(!formData.username || !formData.email || !formData.password)
-      {
-        return setErrorMessage('Please fill out these fields');
-      }
+    if (!formData.username || !formData.email || !formData.password) {
+      return setErrorMessage('Please fill out these fields');
+    }
     try {
       setLoading(true);
       setErrorMessage(null);
-      const res = await fetch('/api/auth/signup' , {
+      const res = await fetch('/api/auth/signup', {
         method: 'POST',
-        headers: {'Content-Type': 'application/json'},
+        headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(formData),
       });
       const data = await res.json();
-      if(data.success === false)
-        {
-          return setErrorMessage(data.message);
-        }
-        setLoading(false);
-        if(res.ok)
-          {
-            navigate('/sign-in');
-          }
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+      if (res.ok) {
+        navigate('/sign-in');
+      }
     } catch (error) {
-      setErrorMessage(error,message);
+      setErrorMessage(error, message);
       setLoading(false);
     }
   };
@@ -47,53 +44,53 @@ export default function SignUp() {
       <div className='flex p-3 max-w-3xl mx-auto flex-col md:flex-row md: items-center gap-5'>
 
         <div className='flex-1'>
-        <Link to= "/" className= '  font-bold dark:text-white text-4xl'>
-      <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'> Vaibhav's</span>
-      Blog
-      </Link>
-      <p className='text-sm mt-5'>
-          This is a demo project. You can sign up with with your email and password
-          or with Google.
-      </p>
+          <Link to="/" className='  font-bold dark:text-white text-4xl'>
+            <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>Our</span>
+            Blog
+          </Link>
+          <p className='text-sm mt-5'>
+            This is a demo project. You can sign up with with your email and password
+            or with Google.
+          </p>
         </div>
 
         <div className='flex-1'>
           <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
             <div>
-               <Label value='Your username'/>
-               <TextInput
-               type='text'
-               placeholder='Username'
-               id='username' onChange={handleChange} />
+              <Label value='Your username' />
+              <TextInput
+                type='text'
+                placeholder='Username'
+                id='username' onChange={handleChange} />
             </div>
 
             <div>
-               <Label value='Your email'/>
-               <TextInput
-               type='email'
-               placeholder='name@company.com'
-               id='email' onChange={handleChange}/>
+              <Label value='Your email' />
+              <TextInput
+                type='email'
+                placeholder='name@company.com'
+                id='email' onChange={handleChange} />
             </div>
 
             <div>
-               <Label value='Your password'/>
-               <TextInput
-               type='password'
-               placeholder='Password'
-               id='password' onChange={handleChange} />
+              <Label value='Your password' />
+              <TextInput
+                type='password'
+                placeholder='Password'
+                id='password' onChange={handleChange} />
             </div>
-             
-             <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
+
+            <Button gradientDuoTone='purpleToPink' type='submit' disabled={loading}>
               {
                 loading ? (
                   <>
-                  <Spinner size='sm'/>
-                  <span className='pl-3'> Loading...</span>
+                    <Spinner size='sm' />
+                    <span className='pl-3'> Loading...</span>
                   </>
                 ) : ('Sign Up')
               }
-             </Button>
-             <OAuth />
+            </Button>
+            <OAuth />
           </form>
           <div className="flex gap-2 text-sm mt-5">
             <span>Have an account?</span>
@@ -108,8 +105,8 @@ export default function SignUp() {
               </Alert>
             )
           }
+        </div>
       </div>
-    </div>
     </div>
   )
 }
