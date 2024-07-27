2ba4cfcbcdd0b0a572e50713b09a03f546a381b2
HeatblasterAA <amanarora_60@yahoo.com>
2024-07-27 15:51:23 +0530

    created get post api route

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,41 +1,49 @@
-import React from 'react'
-import { Sidebar } from 'flowbite-react'
-import {HiUser} from 'react-icons/hi'
-import { HiArrowSmRight } from 'react-icons/hi'
-import { useEffect,useState } from 'react'
-import { Link, useLocation } from 'react-router-dom'
+import React, { useEffect, useState } from 'react';
+import { Sidebar } from 'flowbite-react';
+import { HiDocumentText, HiUser, HiArrowSmRight } from 'react-icons/hi';
+import { Link, useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 export default function DashSidebar() {
-    const location = useLocation()
-  const [tab,setTab] = useState('');
-    useEffect( () => {
-      const urlParams = new URLSearchParams(location.search)
-      const tabfromUrl = urlParams.get('tab')
-      if(tabfromUrl)
-        {
-          setTab(tabfromUrl);
-        }
-    }, [location.search]);
+  const location = useLocation();
+  const [tab, setTab] = useState('');
+  const { currentUser } = useSelector((state) => state.user);
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get('tab');
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.search]);
+
   return (
-    <Sidebar className='w-full md:w-56'>
-       <Sidebar.Items>
+    <Sidebar className="w-full md:w-56">
+      <Sidebar.Items>
         <Sidebar.ItemGroup>
-            <Link to='/dashboard?tab=profile'>
-
-            <Sidebar.Item active={tab === 'profile'} icon = {HiUser} 
-            label={"User"} 
-            labelColor ='dark'
-            as='div'
+          <Link to="/dashboard?tab=profile">
+            <Sidebar.Item
+              active={tab === 'profile'}
+              icon={HiUser}
+              label={currentUser.isAdmin ? 'Admin' : 'User'}
+              labelColor="dark"
+              as="div"
             >
-                Profile
+              Profile
             </Sidebar.Item>
+          </Link>
+          {currentUser.isAdmin && (
+            <Link to="/dashboard?tab=posts">
+              <Sidebar.Item active={tab === 'posts'} icon={HiDocumentText} as="div">
+                Posts
+              </Sidebar.Item>
             </Link>
-
-            <Sidebar.Item  icon = {HiArrowSmRight} className='cursor-pointer'>
-                Sign Out
-            </Sidebar.Item>
+          )}
+          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+            Sign Out
+          </Sidebar.Item>
         </Sidebar.ItemGroup>
-        </Sidebar.Items> 
+      </Sidebar.Items>
     </Sidebar>
-  )
+  );
 }
--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -2,25 +2,26 @@ import React, { useEffect, useState } from 'react'
 import { useLocation } from 'react-router-dom'
 import DashSidebar from '../components/DashSidebar';
 import DashProfile from '../components/DashProfile';
+import DashPosts from '../components/DashPost';
 
 
 export default function Dashboard() {
   const location = useLocation()
-  const [tab,setTab] = useState('');
-    useEffect( () => {
-      const urlParams = new URLSearchParams(location.search)
-      const tabfromUrl = urlParams.get('tab')
-      if(tabfromUrl)
-        {
-          setTab(tabfromUrl);
-        }
-    }, [location.search]);
+  const [tab, setTab] = useState('');
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search)
+    const tabfromUrl = urlParams.get('tab')
+    if (tabfromUrl) {
+      setTab(tabfromUrl);
+    }
+  }, [location.search]);
   return (
-      <div className="min-h-screen flex flex-col md:flex-row">
-        <div className='md:w-56'>
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className='md:w-56'>
         <DashSidebar />
       </div>
-      {tab === 'profile' && <DashProfile />} 
+      {tab === 'profile' && <DashProfile />}
+      {tab === 'posts' && <DashPosts />}
     </div>
   )
 }
