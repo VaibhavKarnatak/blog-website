00e609b22f026b74fb2c26a6307737b679562072
aakaaashmann <aks5060708090100@gmail.com>
2024-08-14 13:02:52 +0530

    Add Like functionality to the comment section

--- api/routes/user.routes.js
+++ api/routes/user.routes.js
@@ -1,20 +1,21 @@
-import express from "express";
-import { getUser } from '../controllers/user.controller.js'; // Adjust the path if necessary
+import express from 'express';
 import {
+  deleteUser,
+  getUser,
+  getUsers,
+  signout,
   test,
   updateUser,
-  deleteUser,
-  signout,
-  getUsers,
-} from "../controllers/user.controller.js";
-import { verifyToken } from "../utils/verifyUser.js";
+} from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
 
 const router = express.Router();
 
-router.get("/test", test);
-router.put("/update/:userId", verifyToken, updateUser);
-router.delete("/delete/:userId", verifyToken, deleteUser);
-router.post("/signout", signout);
-router.get("/getusers", verifyToken, getUsers);
+router.get('/test', test);
+router.put('/update/:userId', verifyToken, updateUser);
+router.delete('/delete/:userId', verifyToken, deleteUser);
+router.post('/signout', signout);
+router.get('/getusers', verifyToken, getUsers);
 router.get('/:userId', getUser);
+
 export default router;
