83b199aecaee0adf6f69bf137f097c9668ccd1c3
aakaaashmann <aks5060708090100@gmail.com>
2024-08-26 17:50:52 +0530

    Completed About and projects pages

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import CallToAction from '../components/CallToAction';
 
 export default function Projects() {
   return (
-    <div>
-      Projects
+    <div className='min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3'>
+      <h1 className='text-3xl font-semibold'>Pojects</h1>
+      <p className='text-md text-gray-500'>You can take a look on our different projects like - Car Rental , Book Recommender</p>
+      <CallToAction />
     </div>
   )
 }
